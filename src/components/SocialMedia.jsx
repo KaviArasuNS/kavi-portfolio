3bbcd5ca397ec66240e1c1d236b8fbe6fa3f9c1c
KaviArasu_NS <KaviArasu_NS@icloud.com>
2022-08-16 04:19:07 +0530

    myportfolio

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+
+const SocialMedia = () => {
+  return (
+    <div className="app__social">
+    <div>
+      <a href="https://www.twitter.com/kaviarasu_ns"><BsTwitter/></a> 
+    </div>
+    <div>
+      <FaFacebookF />
+    </div>
+    <div>
+      <BsInstagram />
+    </div>
+  </div>
+  )
+}
+
+export default SocialMedia
