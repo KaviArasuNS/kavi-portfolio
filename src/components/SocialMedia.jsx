1e1921caa2aa589820f02099362615449b3c1ba6
KaviArasu_NS <KaviArasu_NS@icloud.com>
2022-09-20 20:30:25 +0530

    profile update

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import React from "react";
+import { BsTwitter, BsInstagram } from "react-icons/bs";
+// import { FaFacebookF } from "react-icons/fa";
+// import { FaGithub } from "react-icons/fa";
+import { BsGithub } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
-    <div>
-      <a href="https://www.twitter.com/kaviarasu_ns"><BsTwitter/></a> 
+      <div>
+        <a href="https://github.com/kaviarasuns" target="_blank">
+          <BsGithub />
+        </a>
+      </div>
+      <div>
+        <a href="https://www.twitter.com/kaviarasu_ns" target="_blank">
+          <BsTwitter />
+        </a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/kaviarasuns/" target="_blank">
+          <BsLinkedin />
+        </a>
+      </div>
     </div>
-    {/* <div>
-      <FaFacebookF />
-    </div>
-    <div>
-      <BsInstagram />
-    </div> */}
-  </div>
-  )
-}
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;
--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,12 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import './About.scss';
-import { motion } from 'framer-motion';
-import { urlFor, client } from '../../client';
-import AppWrap from '../../wrapper/AppWrap';
-import MotionWrap from '../../wrapper/MotionWrap';
-
-
-
+import React, { useEffect, useState } from "react";
+import "./About.scss";
+import { motion } from "framer-motion";
+import { urlFor, client } from "../../client";
+import AppWrap from "../../wrapper/AppWrap";
+import MotionWrap from "../../wrapper/MotionWrap";
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
@@ -21,30 +18,36 @@ const About = () => {
 
   return (
     <>
-    <h2 className="head-text">I Know that <span>Good Development</span> <br />means  <span>Good Business</span></h2>
-    
-    <div className="app__profiles">
+      <h2 className="head-text">
+        A journey of a <span>Thousand Miles</span> <br />
+        begins with a <span>Single Step</span>
+      </h2>
+
+      <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: 'tween' }}
+            transition={{ duration: 0.5, type: "tween" }}
             className="app__profile-item"
             key={about.title + index}
           >
             <img src={urlFor(about.imgUrl)} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
-            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
           </motion.div>
         ))}
       </div>
-
     </>
-  )
-}
+  );
+};
 
 export default AppWrap(
-  MotionWrap(About, 'app__about'),
-  'about',
-  'app__whitebg',
+  MotionWrap(About, "app__about"),
+  "about",
+  "app__whitebg"
 );
